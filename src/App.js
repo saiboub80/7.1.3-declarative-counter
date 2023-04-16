3a60e05b483807465e719e5895d0b3ab3354b012
Saibou <saiboub80@gmail.com>
2023-04-16 13:00:50 -0400

    creation of react firststep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+//import react and useState together
+import React, { useState } from 'react'
 function App() {
+  //start point
+  const [counter, changeCounter] = useState(1)
+  //without the window it work I try
+  window.changeCounter = changeCounter
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
     </div>
   );
 }
